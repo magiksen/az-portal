771792eff8c135bb0ca7f6b3aba84771f05be4ef
magiksen <elemejore@gmail.com>
2023-02-15 09:45:03 -0400

    Admin profile & image update part 1

--- resources/views/admin/admin_profile_view.blade.php
+++ resources/views/admin/admin_profile_view.blade.php
@@ -1,4 +1,26 @@
 @extends('admin.admin_master')
 @section('admin')
 
+    <div class="page-content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="card">
+                        <div class="p-4" style="text-align: center;">
+                            <img class="rounded-circle avatar-xl" src="{{ asset('backend/assets/images/small/img-5.jpg') }}" alt="Card image cap">
+                        </div>
+                        <div class="card-body">
+                            <h4 class="card-title">Nombre: {{ $adminData->name }}</h4>
+                            <hr>
+                            <h4 class="card-title">Email: {{ $adminData->email }}</h4>
+                            <hr>
+                            <h4 class="card-title">Usuario: {{ $adminData->username }}</h4>
+                            <hr>
+                            <a href="#" class="btn btn-info btn-rounded waves-effect waves-light">Editar Perfil</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
