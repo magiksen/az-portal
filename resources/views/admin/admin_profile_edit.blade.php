e51dac8d276a23dc549ba0d173c6b9b015ed306f
magiksen <elemejore@gmail.com>
2023-02-15 12:38:17 -0400

    Admin Profile & Image Update 4

--- resources/views/admin/admin_profile_edit.blade.php
+++ resources/views/admin/admin_profile_edit.blade.php
@@ -9,47 +9,49 @@
                     <div class="card">
                         <div class="card-body">
                             <h4 class="card-title">Actualizar Perfil</h4>
-
-                            <div class="row mb-3">
-                                <label for="name" class="col-sm-2 col-form-label">Nombre</label>
-                                <div class="col-sm-10">
-                                    <input class="form-control" type="text" name="name" id="name" value="{{ $editData->name }}">
+                            <form method="post" action="{{ route('store.profile') }}" enctype="multipart/form-data">
+                                @csrf
+                                <div class="row mb-3">
+                                    <label for="name" class="col-sm-2 col-form-label">Nombre</label>
+                                    <div class="col-sm-10">
+                                        <input class="form-control" type="text" name="name" id="name" value="{{ $editData->name }}">
+                                    </div>
                                 </div>
-                            </div>
-                            <!-- end row -->
+                                <!-- end row -->
 
-                            <div class="row mb-3">
-                                <label for="email" class="col-sm-2 col-form-label">Email</label>
-                                <div class="col-sm-10">
-                                    <input class="form-control" type="text" name="email" id="email" value="{{ $editData->email }}">
+                                <div class="row mb-3">
+                                    <label for="email" class="col-sm-2 col-form-label">Email</label>
+                                    <div class="col-sm-10">
+                                        <input class="form-control" type="text" name="email" id="email" value="{{ $editData->email }}">
+                                    </div>
                                 </div>
-                            </div>
-                            <!-- end row -->
+                                <!-- end row -->
 
-                            <div class="row mb-3">
-                                <label for="username" class="col-sm-2 col-form-label">Usuario</label>
-                                <div class="col-sm-10">
-                                    <input class="form-control" type="text" name="username" id="username" value="{{ $editData->username }}">
+                                <div class="row mb-3">
+                                    <label for="username" class="col-sm-2 col-form-label">Usuario</label>
+                                    <div class="col-sm-10">
+                                        <input class="form-control" type="text" name="username" id="username" value="{{ $editData->username }}">
+                                    </div>
                                 </div>
-                            </div>
-                            <!-- end row -->
+                                <!-- end row -->
 
-                            <div class="row mb-3">
-                                <label for="profile_image" class="col-sm-2 col-form-label">Imagen de Perfil</label>
-                                <div class="col-sm-10">
-                                    <input class="form-control" type="file" name="profile_image" id="profile_image">
+                                <div class="row mb-3">
+                                    <label for="profile_image" class="col-sm-2 col-form-label">Imagen de Perfil</label>
+                                    <div class="col-sm-10">
+                                        <input class="form-control" type="file" name="profile_image" id="profile_image">
+                                    </div>
                                 </div>
-                            </div>
-                            <!-- end row -->
+                                <!-- end row -->
 
-                            <div class="row mb-3">
-                                <label class="col-sm-2 col-form-label"> </label>
-                                <div class="col-sm-10">
-                                    <img id="showImage" class="rounded avatar-lg" src="{{ asset('backend/assets/images/small/img-5.jpg') }}" alt="Card image cap">
+                                <div class="row mb-3">
+                                    <label class="col-sm-2 col-form-label"> </label>
+                                    <div class="col-sm-10">
+                                        <img id="showImage" class="rounded avatar-lg" src="{{ asset('backend/assets/images/small/img-5.jpg') }}" alt="Card image cap">
+                                    </div>
                                 </div>
-                            </div>
-                            <!-- end row -->
-                            <input type="submit" class="btn btn-info waves-effect waves-light" value="Actualizar Perfil">
+                                <!-- end row -->
+                                <input type="submit" class="btn btn-info waves-effect waves-light" value="Actualizar Perfil">
+                            </form>
                         </div>
                     </div>
                 </div> <!-- end col -->
